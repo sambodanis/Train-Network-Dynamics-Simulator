c34a7b5735d4a3c698edad16b9d7adfd1e3c4301
sambodanis <sam.bodanis@gmail.com>
2014-04-03 20:41:27 +0100

    degree failure plot

--- visualisations.py
+++ visualisations.py
@@ -51,25 +51,78 @@ import matplotlib.pyplot as plt
 # fig.suptitle('Random engineering failures', fontsize=18, fontweight='bold')
 # plt.show()
 
+# aggregate = []
+# station_list = []
+# examples = 100
+# for i in range(examples):
+#     filename = 'simulation_results/line_fail' + str(i) + '.txt'
+#     f = open(filename, 'r').read().split('\n')[:-2]
+#     if not aggregate:
+#         aggregate = [(0, 0, 0)] * len(f)
+#         station_list = [x[1:-1].split(', ')[-1] for x in f]
+#     for j, tup in enumerate(f):
+#         t = tup[1:-1].split(', ')
+#         aggregate[j] = [aggregate[j][x] + float(
+#                         t[x]) for x in range(len(t) - 1)]
+
+# aggregate = zip(
+#     map(lambda x: (x[0] / examples, (x[1] / examples) / 10), aggregate), station_list)
+# for i, a in enumerate(aggregate):
+#     aggregate[i] = (a[0][0], a[0][1], a[1][1:-1])
+# aggregate.sort(key=lambda x: x[1])
+
+# fig, ax = plt.subplots()
+
+# axes = [ax, ax.twinx()]
+# width = 0.35
+# colors = ['Red', 'Blue']
+# labels = ['Average travel time', 'Commuters who get to work']
+# units = ['Minutes', 'Percentage']
+# rects1 = axes[0].bar(range(len(aggregate)),
+#              [x[0] for x in aggregate], width, color='r')
+
+# rects2 = axes[1].bar(map(lambda x: x + width, range(len(aggregate))),
+#              [x[1] for x in aggregate], width, color='b')
+# ax.set_xticklabels([x[2] for x in aggregate])
+# ax.set_xticks(map(lambda x: x + width, range(len(aggregate))))
+# for tick in ax.xaxis.get_major_ticks():
+#     tick.label.set_fontsize(11)
+# plt.legend([rects1, rects2], labels, loc='lower right')
+# axes[0].set_ylim(35, 39)
+# axes[1].set_ylim(65, 100)
+# axes[0].set_ylabel(units[0], color=colors[0])
+# axes[1].set_ylabel(units[1], color=colors[1])
+# ax.set_xlabel('Line')
+# fig.suptitle('Line Failures', fontsize=18, fontweight='bold')
+# plt.show()
+
+
 aggregate = []
-station_list = []
-examples = 100
+examples = 5
 for i in range(examples):
-    filename = 'simulation_results/line_fail' + str(i) + '.txt'
+    filename = 'simulation_results/fail_degree' + str(i) + '.txt'
     f = open(filename, 'r').read().split('\n')[:-2]
     if not aggregate:
-        aggregate = [(0, 0, 0)] * len(f)
-        station_list = [x[1:-1].split(', ')[-1] for x in f]
+        aggregate = [[0, 0, 0]] * len(f)
     for j, tup in enumerate(f):
         t = tup[1:-1].split(', ')
         aggregate[j] = [aggregate[j][x] + float(
-                        t[x]) for x in range(len(t) - 1)]
+                        t[x]) for x in range(len(t) - 2)] + [int(t[2]) + int(t[3])]
 
-aggregate = zip(
-    map(lambda x: (x[0] / examples, (x[1] / examples) / 10), aggregate), station_list)
-for i, a in enumerate(aggregate):
-    aggregate[i] = (a[0][0], a[0][1], a[1][1:-1])
-aggregate.sort(key=lambda x: x[1])
+aggregate = map(
+    lambda x: (x[0] / examples, (x[1] / examples) / 10, x[2]), aggregate)
+aggregate.sort(key=lambda x: x[2])
+freqs = [None] * max(aggregate, key=lambda x: x[2])[2]
+
+for x in aggregate:
+    if not freqs[x[2] - 1]:
+        freqs[x[2] - 1] = ([x[0]], [x[1]])
+    else:
+        freqs[x[2] - 1][0].append(x[0])
+        freqs[x[2] - 1][1].append(x[1])
+
+aggregate = map(
+    lambda x: (sum(x[0]) / len(x[0]), sum(x[1]) / len(x[1])), freqs)
 
 fig, ax = plt.subplots()
 
@@ -83,15 +136,16 @@ rects1 = axes[0].bar(range(len(aggregate)),
 
 rects2 = axes[1].bar(map(lambda x: x + width, range(len(aggregate))),
              [x[1] for x in aggregate], width, color='b')
-ax.set_xticklabels([x[2] for x in aggregate])
+ax.set_xticklabels([i + 1 for i, x in enumerate(aggregate)])
 ax.set_xticks(map(lambda x: x + width, range(len(aggregate))))
 for tick in ax.xaxis.get_major_ticks():
     tick.label.set_fontsize(11)
 plt.legend([rects1, rects2], labels, loc='lower right')
-axes[0].set_ylim(35, 39)
-axes[1].set_ylim(65, 100)
+axes[0].set_ylim(37.4, 37.7)
+axes[1].set_ylim(98, 100)
 axes[0].set_ylabel(units[0], color=colors[0])
 axes[1].set_ylabel(units[1], color=colors[1])
-ax.set_xlabel('Line')
-fig.suptitle('Line Failures', fontsize=18, fontweight='bold')
+ax.set_xlabel('Degree sum of adjacient stations')
+fig.suptitle('Link removal by end node degree sum',
+             fontsize=18, fontweight='bold')
 plt.show()
